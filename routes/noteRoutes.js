77765a5b2e202d8330da4e795a935e603f042ada
Tobi <467bnguyen@gmail.com>
2020-08-20 21:43:51 -0700

    finished code

---
+++ routes/noteRoutes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const { join } = require('path');
+const fs = require('fs');
+const uuid = require('uuid');
+
+router.get('/notes', (req, res) => {
+    fs.readFile(join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+        if (err) { console.log(err) }
+        res.json(JSON.parse(data));
+    })
+})
+
+router.post('/notes', (req, res) => {
+    fs.readFile(join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+        if (err) { console.log(err) }
+
+        let notes = JSON.parse(data);
+        let note = {
+            id: uuid.v1(),
+            title: req.body.title,
+            text: req.body.text
+        };
+        notes.push(note);
+
+        fs.writeFile(join(__dirname, '..', 'db', 'db.json'), JSON.stringify(notes), err => {
+            if (err) { console.log(err) }
+            res.json(note);
+        })
+    })
+})
+  
+router.delete('/notes/:id', (req, res) => {
+
+    fs.readFile(join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
+        if (err) { console.log(err) }
+    
+        let notes = JSON.parse(data)
+        notes = notes.filter(note => note.id !== req.params.id)
+    
+        fs.writeFile(join(__dirname, '..', 'db', 'db.json'), JSON.stringify(notes), err => {
+            if (err) { console.log(err) }
+            
+            res.send('Note Deleted!');
+        })
+    })
+})
+
+module.exports = router;
